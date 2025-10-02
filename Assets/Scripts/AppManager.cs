be3c97699d9f93c112a9811b59f6cd146b96968f
Henri Coeuret <henri.coeuret@kubapay.com>
2025-10-02 15:22:02 +0200

    Add Scenes squeleton

---
+++ Assets/Scripts/AppManager.cs
@@ -0,0 +1,30 @@
+using UnityEditor.SearchService;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static AppManager Instance { get; private set; }
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject );
+            return;
+        }
+
+        Instance = this;
+
+        DontDestroyOnLoad(gameObject);
+
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Initialize()
+    {
+        Debug.Log("Initializing Map");
+        SceneManager.LoadScene("MapScene");
+    }
+}
