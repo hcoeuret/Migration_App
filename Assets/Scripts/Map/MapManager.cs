d795aab3db0b7e4057800724c8bdabf78b1b64ee
Henri Coeuret <henri.coeuret@kubapay.com>
2025-10-16 15:45:29 +0200

    Instanciate cross on click for future position

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class MapManager : MonoBehaviour
 {
     [SerializeField] GameObject crossPf;
+    [SerializeField] GameObject crossFuturePf;
     [SerializeField] Camera cam;
+    [SerializeField] LayerMask groundLayer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,7 +14,7 @@ public class MapManager : MonoBehaviour
         Vector3 spawnPosition = new Vector3(xPos, yPos, 0f);
         Quaternion rotation = Quaternion.Euler(0f, 45f, 0f);
 
-        Vector3 cameraPos = spawnPosition + new Vector3(1.5f, 40, -20);
+        Vector3 cameraPos = spawnPosition + new Vector3(0, 40, -20);
         Instantiate(crossPf, spawnPosition, rotation);
         cam.transform.position = cameraPos;
     }
@@ -20,6 +22,18 @@ public class MapManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red, 4f);
+            if (Physics.Raycast(ray, out RaycastHit hit, 100f, groundLayer))
+            {
+                Vector3 clickPos = hit.point;
+                Debug.Log("Clicked at: " + clickPos);
+                Vector3 futurePos = clickPos;
+                Quaternion rotation = Quaternion.Euler(0f, 45f, 0f);
+                Instantiate(crossFuturePf, futurePos, rotation);
+            }
+        }
     }
 }
